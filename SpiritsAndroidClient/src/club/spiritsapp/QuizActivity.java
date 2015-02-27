1ee8890a710cdc6da169f4ab45e07169bd627245
Lance Nanek <lnanek@gmail.com>
2015-02-27 15:44:48 -0800

    Hello World Android client.

---
+++ SpiritsAndroidClient/src/club/spiritsapp/QuizActivity.java
@@ -0,0 +1,15 @@
+package club.spiritsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuizActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_quiz);
+	}
+
+}
