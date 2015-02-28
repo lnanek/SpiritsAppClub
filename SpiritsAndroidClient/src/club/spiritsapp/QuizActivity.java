319e3c7ca249fa82e5600f6b2f6a7cc4532cd455
Lance Nanek <lnanek@gmail.com>
2015-02-27 18:09:57 -0800

    Show checkbox for each type.

--- SpiritsAndroidClient/src/club/spiritsapp/QuizActivity.java
+++ SpiritsAndroidClient/src/club/spiritsapp/QuizActivity.java
@@ -1,22 +1,21 @@
 package club.spiritsapp;
 
-import java.lang.reflect.Type;
-import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 
-import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class QuizActivity extends Activity {
+	
+	private static final String TAG = QuizActivity.class.getSimpleName();
 
     private final Gson gson = new Gson();
 	
@@ -30,13 +29,15 @@ public class QuizActivity extends Activity {
 /varietals
 	 */
 
+	private ViewGroup typesContainer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_quiz);
 		
-		final TextView mTextView = (TextView) findViewById(R.id.prompt);
+		typesContainer = (ViewGroup) findViewById(R.id.typesContainer);
 
 		// Instantiate the RequestQueue.
 		RequestQueue queue = Volley.newRequestQueue(this);
@@ -49,17 +50,34 @@ public class QuizActivity extends Activity {
 		        // Display the first 500 characters of the response string.
 		        //mTextView.setText("Response is: "+ response);
 		        
-		        mTextView.setText("Response is: "+ response);
+		        //mTextView.setText("Response is: "+ response);
+		    	
+		    	displayTypes(response);
 		        
 		    }
 		}, new Response.ErrorListener() {
 		    @Override
 		    public void onErrorResponse(VolleyError error) {
-		        mTextView.setText("That didn't work!");
+		    	Log.e(TAG, "Error downloading types: ", error);
+		        //mTextView.setText("That didn't work!");
 		    }
 		});
 		// Add the request to the RequestQueue.
 		queue.add(stringRequest);
+		
+	}
+	
+	private void displayTypes(TypesResponse types) {
+		
+		final LayoutInflater inflator = getLayoutInflater();
+		
+		for(final VarietalType type : types.types) {
+			
+			final CheckBox checkbox = (CheckBox) inflator.inflate(R.layout.activity_quiz_type, typesContainer, false);
+			checkbox.setText(type.name);
+			typesContainer.addView(checkbox);
+						
+		}
 		
 	}
 
