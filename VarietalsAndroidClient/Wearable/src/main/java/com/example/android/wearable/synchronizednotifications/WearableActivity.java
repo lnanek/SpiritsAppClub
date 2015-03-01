8b4692f2ba1770e926cc4403a0a499531f002205
Lance Nanek <lnanek@gmail.com>
2015-02-28 16:32:01 -0800

    Swipe to rate demo on watch.

--- VarietalsAndroidClient/Wearable/src/main/java/com/example/android/wearable/synchronizednotifications/WearableActivity.java
+++ VarietalsAndroidClient/Wearable/src/main/java/com/example/android/wearable/synchronizednotifications/WearableActivity.java
@@ -17,14 +17,74 @@ package com.example.android.wearable.synchronizednotifications;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import club.spiritsapp.R;
 
 public class WearableActivity extends Activity {
 
+    private static final String TAG = WearableActivity.class.getSimpleName();
+
+    ViewGroup wineRating;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wearable);
+
+        wineRating = (ViewGroup) findViewById(R.id.wineRating);
+        final int wineImageCount = wineRating.getChildCount();
+
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setWineRating(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void setWineRating(int progress) {
+
+        final float progressPercent = (float) progress / 100f;
+        Log.i(TAG, "setWineRating progress = " + progressPercent);
+
+        final int childCount = wineRating.getChildCount();
+        final float progressPercentPerChild = 1.0f / childCount;
+
+        for(int childIndex = 0 ; childIndex < childCount; childIndex++ ) {
+
+            final ImageView currentChild = (ImageView) wineRating.getChildAt(childIndex);
+
+            final float progressPercentToBeEmpty = progressPercentPerChild * (childIndex);
+            final float progressPercentToBeFull = progressPercentPerChild * (childIndex + 1);
+
+
+            Log.i(TAG, "setWineRating child = " + childIndex
+                    + " progressPercentToBeEmpty = " + progressPercentToBeEmpty
+                    + " progressPercentToBeFull = " + progressPercentToBeFull);
+
+            if (progressPercent >= progressPercentToBeFull) {
+                currentChild.setImageResource(R.drawable.ic_full_glass);
+            } else if (progressPercent > progressPercentToBeEmpty) {
+                currentChild.setImageResource(R.drawable.ic_half_glass);
+            } else {
+                currentChild.setImageResource(R.drawable.ic_empty_glass);
+            }
+        }
+
     }
 }
